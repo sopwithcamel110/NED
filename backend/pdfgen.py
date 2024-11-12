4c8f77fb863d3f8b491930e6d0fc5a7514451f7d
Donovan deVise <ddevise@ucsc.edu>
2024-11-11 19:47:06 -0800

    Clarify

--- backend/pdfgen.py
+++ backend/pdfgen.py
@@ -3,10 +3,12 @@ from io import BufferedWriter
 import os
 import random
 import tempfile
-from typing import Any, Dict, List, Tuple
 import textwrap
 
-from reportlab.platypus import SimpleDocTemplate, Image, Spacer
+from PIL import Image 
+from typing import Any, Dict, List, Tuple
+
+from reportlab.platypus import SimpleDocTemplate, Spacer
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
@@ -22,16 +24,20 @@ class MediaType(Enum):
     TEXT = "text"
     IMAGE = "image"
 
-def get_dimensions(c: canvas.Canvas, content: ContentType, font_size: int, max_width: float) -> Tuple[float, float]:
+def wrap_string_list(strs: List[str]) -> List[str]:
+    res = []
+    for s in strs:
+        res.extend(textwrap.wrap(s))
+    return res
+
+def get_dimensions(c: canvas.Canvas, content: ContentType, font_size: int,) -> Tuple[float, float]:
     """
     Calculate the width and height of the given content, considering text wrapping.
-
     Parameters:
     - c: The ReportLab canvas object.
     - content: The content dictionary containing topic, content, and media type.
     - font_size: Font size for text.
     - max_width: Maximum width of the text content before wrapping.
-
     Returns:
     - A tuple containing:
         - The maximum width of the content.
@@ -40,14 +46,26 @@ def get_dimensions(c: canvas.Canvas, content: ContentType, font_size: int, max_w
     match MediaType(content['media']):
         case MediaType.TEXT:
             topic = content['topic']
-            bullet_points = content['content']
+            bullet_points = wrap_string_list(content['content'])
+            content['wrapped_content'] = bullet_points # Update content for future use
+
             topic_width = max(c.stringWidth(s, "Topic-Font", font_size) for s in [topic] + bullet_points) 
-            topic_height = (len(bullet_points)+1) # +1 for topic
-            return (topic_width * 0.70), (topic_height * 1.75)
+            topic_height = font_size * (len(bullet_points)+1) # +1 for topic
+            return topic_width, topic_height
         case MediaType.IMAGE:
-            image_width = content['content'][1] * inch
-            image_height = content['content'][2] * inch
-            return image_width, (image_height // 5)
+            #content width, content height
+            img = Image.open(content['content'][0]) 
+  
+            # get width and height 
+            image_width = img.width 
+            image_height = img.height
+            scale_factor = max(image_height, image_height) / (2*inch)
+            image_height /= scale_factor
+            image_width /= scale_factor
+
+            content['content'][1] = image_width
+            content['content'][2] = image_height
+            return image_width, image_height
 
 def place_content(
     c: canvas.Canvas,
@@ -58,7 +76,6 @@ def place_content(
 ) -> None:
     """
     Place content directly at x and y on the given canvas.
-
     Parameters:
     - c: The ReportLab canvas object.
     - content: The content dictionary containing topic, content, and media type.
@@ -69,28 +86,22 @@ def place_content(
     match MediaType(content['media']):
         case MediaType.TEXT:
             topic = content['topic']
-            bullet_points = content['content']  # List of bullet points
-            
+            bullet_points = content['wrapped_content']
+
             # Draw the topic in bold
             c.setFont("Topic-Font", font_size)
             c.drawString(x, y, topic)
-            y -= font_size
 
-            # Draw content within width specified for text wrapping
-            c.setFont("Bullet-Font", font_size)
-            for bullet in bullet_points:
-                wrapper = textwrap.wrap(bullet, width=font_size * 10)
-                c.drawString(x, y, '•')
-                for line in wrapper:
-                    c.drawString(x + 3, y, line)
-                    y -= font_size
+            c.setFont("Bullet-Font", font_size)  # Smaller font size to fit more text
+            for i, bullet in enumerate(bullet_points):
+                # Draw bullet point
+                c.drawString(x, y-((i+1)*font_size), bullet)
 
         case MediaType.IMAGE:
             image_path = content['content'][0]
-            image_width = content['content'][1] * inch
-            image_height = content['content'][2] * inch
+            image_width = content['content'][1]
+            image_height = content['content'][2]
             c.drawImage(image_path, x, y - image_height, width=image_width, height=image_height)
-
 
 def create_pdf(data_dict: List[ContentType], file: BufferedWriter) -> None:
     """
@@ -108,21 +119,21 @@ def create_pdf(data_dict: List[ContentType], file: BufferedWriter) -> None:
     total_height = 0
     msf_height = 0 # max-so-far height
     for content in data_dict:
-        topic_width, topic_height = get_dimensions(c, content, font_size, font_size * 20)
+        topic_width, topic_height = get_dimensions(c, content, font_size)
 
         if current_x + topic_width > page_width:
             # Topic goes off right of page, go to next line
             current_x = 0
             total_height += msf_height
             msf_height = 0
-            current_y = page_height-(total_height*font_size)-top_margin  # Move down to the next line
+            current_y = page_height-top_margin-total_height  # Move down to the next line
         elif current_y - (topic_height*font_size) <= 0:
             # Topic goes off bottom of page, create a new one
             c.showPage()
             total_height = msf_height = 0
             current_x = 0
             current_y = page_height-top_margin 
-        
+
         msf_height = max(msf_height, topic_height)
 
         place_content(c, content, current_x, current_y, font_size)
@@ -132,47 +143,6 @@ def create_pdf(data_dict: List[ContentType], file: BufferedWriter) -> None:
 
     # Save the PDF
     c.save()
-
-# def create_pdf(data_dict: List[ContentType], file: BufferedWriter) -> None:
-#     """
-#     Create a fully optimized cheatsheet from a list of topics. Utilizes formatting and
-#     positioning to make efficient use of white space.
-#     """
-#     c = canvas.Canvas(file, pagesize=A4)
-#     page_width, page_height = A4
-#     font_size = 5
-    
-#     top_margin = font_size
-#     current_x = 0
-#     current_y = page_height-top_margin
-
-#     total_height = 0
-#     msf_height = 0 # max-so-far height
-#     for content in data_dict:
-#         topic_width, topic_height = get_dimensions(c, content, font_size)
-
-#         if current_x + topic_width > page_width:
-#             # Topic goes off right of page, go to next line
-#             current_x = 0
-#             total_height += msf_height
-#             msf_height = 0
-#             current_y = page_height-(total_height*font_size)-top_margin  # Move down to the next line
-#         elif current_y - (topic_height*font_size) <= 0:
-#             # Topic goes off bottom of page, create a new one
-#             c.showPage()
-#             total_height = msf_height = 0
-#             current_x = 0
-#             current_y = page_height-top_margin 
-
-#         msf_height = max(msf_height, topic_height)
-
-#         place_content(c, content, current_x, current_y, font_size)
-
-#         # Check for page overflow
-#         current_x += topic_width  # Update x position after the topic
-
-#     # Save the PDF
-#     c.save()
 
 def create_cheatsheet_pdf(data_dict: List[ContentType]) -> str:
     """
